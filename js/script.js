65838fa8c43df13dbd64724d0ee8ec4313dc9e15
mdeias <mdeias@icloud.com>
2021-11-10 14:18:25 +0100

    elimino la possibilità che i numeri casuali si ripetano

--- js/script.js
+++ js/script.js
@@ -2,10 +2,7 @@
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// 1 creo una funzione che estrae unmeri casuali da 1 a 100
-// 2 creo una funzione che stampa le caselle 
-// 3 creo un timer contenete la funzione facendo parire il prompt con la domanda all'utente
-// 4 confronto i numeri dell'utente con quelli generati casualmente
+
 
 
 //1
@@ -15,28 +12,36 @@ function numRandom(min, max) {
 }
 
 
-// ciclo for che stampa le caselle
+// recupero il contenitore
 const contenitore = document.querySelector(".container");
+//array che conterrà i numeri casuali generati dal pc
 const numeriPc =[];
+// arrai che conterrà i numeri inseriti dall'utente
 const numeriUtente = [];
+// creo il testo che comparirà assieme ai numeri casuali
 const testo = document.createElement("h1");
 testo.innerHTML = `Memorizza i numeri`;
 contenitore.append(testo);
 
 
 
+// ciclo for che stampa le caselle con i numeri casuali
 for (let i = 0; i < 5; i++) {
     const casella = document.createElement("div");
     casella.classList.add("casella");
     let numeriCasuali = numRandom(1, 100);
     casella.innerHTML = `<h1>${numeriCasuali}</h1>`;
     console.log(casella);
-    numeriPc.push(numeriCasuali);
-    contenitore.append(casella);
+    if (!numeriPc.includes(numeriCasuali)) {
+        numeriPc.push(numeriCasuali);
+        contenitore.append(casella);
+    }
+   
 }
 console.log(numeriPc);
 
 
+// funzione che resetta il contenitore e dopo il tempo stabilito chiede i numeri all'utente confrontandoli con quelli generati dal pc
 
 setTimeout(function(){
     contenitore.innerHTML = "";
